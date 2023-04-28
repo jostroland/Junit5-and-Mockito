2adcd00ea58fa08d9b2a4e9a844d2b9262cd6c20
Jost Roland <jostadama@gmail.com>
2023-04-28 14:53:11 +0000

    Modification du fichier Conditions OS et Jre

--- src/test/java/ConditionsOsJre.java
+++ src/test/java/ConditionsOsJre.java
@@ -1,2 +1,39 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.junit.jupiter.api.condition.OS.*;
+
 public class ConditionsOsJre {
+
+    @Test
+    @EnabledOnOs(MAC)
+    void onlyOnMacOs() {
+        // ...
+    }
+    @TestOnMac
+    void testOnMac() {
+        // ...
+    }
+    @Test
+    @EnabledOnOs({ LINUX, MAC })
+    void onLinuxOrMac() {
+        // ...
+    }
+    @Test
+    @DisabledOnOs(WINDOWS)
+    void notOnWindows() {
+        // ...
+    }
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Test
+    @EnabledOnOs(MAC)
+    @interface TestOnMac {
+    }
 }

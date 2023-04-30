22e47cc28a1f12b18e81e4f6f257098d61b71f03
Jost Roland <jostadama@gmail.com>
2023-04-30 14:33:16 +0000

    Ajout des fichier Conditions OS-JRE et GenerateurDeNom

--- src/test/java/ConditionsOsJre.java
+++ src/test/java/ConditionsOsJre.java
@@ -1,12 +1,12 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.*;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static org.junit.jupiter.api.condition.JRE.*;
 import static org.junit.jupiter.api.condition.OS.*;
 
 public class ConditionsOsJre {
@@ -36,4 +36,73 @@ public class ConditionsOsJre {
     @EnabledOnOs(MAC)
     @interface TestOnMac {
     }
+
+
+
+    @Test
+    @EnabledOnJre(JAVA_8)
+    void onlyOnJava8() {
+        // ...
+    }
+    @Test
+    @EnabledOnJre({ JAVA_9, JAVA_10 })
+    void onJava9Or10() {
+        // ...
+    }
+    /*@Test
+    @EnabledForJreRange(min = JAVA_9, max = JAVA_11)
+    void fromJava9to11() {
+
+    }
+    @Test
+    @EnabledForJreRange(min = JAVA_9)
+    void fromJava9toCurrentJavaFeatureNumber() {
+        // ...
+    }
+    @Test
+    @EnabledForJreRange(max = JAVA_11)
+    void fromJava8To11() {
+        // ...
+    }*/
+    @Test
+    @DisabledOnJre(JAVA_9)
+    void notOnJava9() {
+        // ...
+    }
+   /* @Test
+    @DisabledForJreRange(min = JAVA_9, max = JAVA_11)
+    void notFromJava9to11() {
+        // ...
+    }
+    @Test
+    @DisabledForJreRange(min = JAVA_9)
+    void notFromJava9toCurrentJavaFeatureNumber() {
+        // ...
+    }
+    @Test
+    @DisabledForJreRange(max = JAVA_11)
+    void notFromJava8to11() {
+        // ...
+    }*/
+
+
+    /**
+     * Autres conditions
+     */
+    @Test
+    @EnabledIf("customCondition")
+    void enabled() {
+        // ...
+    }
+
+    @Test
+    @DisabledIf("customCondition")
+    void disabled() {
+        // ...
+    }
+
+    boolean customCondition() {
+        return true;
+    }
+
 }
